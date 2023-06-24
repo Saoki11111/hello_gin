f8c971c4073ccb1e5be10dc5825939564d9568ae
Saoki11111 <t.aoki11111@gmail.com>
2023-06-24 13:20:53 +0900

    フォームの入力された文字を送信 h1 タグがそのまま出力される...

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
-
-
 package main
 import (
   "log"
   "github.com/gin-gonic/gin"
+  "net/http"
 )
 func main() {
   router := gin.Default()
+
+  router.StaticFS("/static", http.Dir("static"))
+
   router.GET("/", func(ctx *gin.Context) {
-    ctx.String(200, "Hello, World!")
+    ctx.Redirect(302, "/static/index.html")
+  })
+
+  router.GET("/hello", func(ctx *gin.Context) {
+    name := ctx.Query("name")
+    ctx.Header("Context-Type", "text/html; charset=UTF-8")
+    ctx.String(200, "<h1>Hello, " + name + "</h1>")
   })
 
   err := router.Run("127.0.0.1:8888")
